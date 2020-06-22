0d52e4d82cf92b3ea21b639ed6547b56be681ef2
Eamon Walsh <ewalsh@termysequence.com>
2020-06-22 00:29:05 -0400

    Use maps to reduce the size of the source

--- STLSoundBoard.lua
+++ STLSoundBoard.lua
@@ -1,14 +1,74 @@
 STLSoundBoard = { }
 
---Druid color: ff7d0a
---Hunter color: a9d271
---Mage color: 40c7eb
---Paladin color: f58cba
---Priest color: ffffff
---Rogue color: fff569
---Shaman color: 0070de
---Warlock color: 8787ed
---Warrior color: c79c6e
+local STLSoundFolder = "Interface\\AddOns\\STLSoundBoard\\Sounds\\"
+local STLSoundChannel = "Master"
+
+local STLClassColors = {
+    ["Druid"] = "ffff7d0a",
+    ["Hunter"] = "ffa9d271",
+    ["Mage"] = "ff40c7eb",
+    ["Paladin"] = "fff58cba",
+    ["Priest"] = "ffffffff",
+    ["Rogue"] = "fffff569",
+    ["Shaman"] = "ff0070de",
+    ["Warlock"] = "ff8787ed",
+    ["Warrior"] = "ffc79c6e",
+}
+
+local STLCharacters = {
+    ["Beveryman"] = "Warrior",
+    ["Bombola"] = "Rogue",
+    ["Crumbles"] = "Hunter",
+    ["Funkyphantom"] = "Warrior",
+    ["Harumph"] = "Warlock",
+    ["Heboric"] = "Priest",
+    ["Luger"] = "Hunter",
+    ["Panserbjørn"] = "Druid",
+    ["Powerthirst"] = "Mage",
+    ["Rawberry"] = "Hunter",
+    ["Resaris"] = "Rogue",
+    ["Shmeeshmaam"] = "Paladin",
+}
+
+local STLCritSounds = {
+    ["Beveryman"] = STLSoundFolder .. "Powerthirst.mp3",
+    ["Bombola"] = STLSoundFolder .. "Zap.mp3",
+    ["Crumbles"] = STLSoundFolder .. "Snip_Snap.mp3",
+    ["Funkyphantom"] = STLSoundFolder .. "Funky.mp3",
+    ["Harumph"] = STLSoundFolder .. "Harumph.mp3",
+    ["Heboric"] = STLSoundFolder .. "GunShow.mp3",
+    ["Luger"] = STLSoundFolder .. "GunShow.mp3",
+    ["Panserbjørn"] = STLSoundFolder .. "Angry_Cat.mp3",
+    ["Powerthirst"] = STLSoundFolder .. "Powerthirst.mp3",
+    ["Rawberry"] = STLSoundFolder .. "Powerthirst.mp3",
+    ["Resaris"] = STLSoundFolder .. "GunShow.mp3",
+    ["Shmeeshmaam"] = STLSoundFolder .. "I_Am_The_Law.mp3",
+}
+
+local STLHealSounds = {
+    ["Heboric"] = STLSoundFolder .. "Hallelujah.mp3",
+    ["Panserbjørn"] = STLSoundFolder .. "Hallelujah.mp3",
+    ["Shmeeshmaam"] = STLSoundFolder .. "Hallelujah.mp3",
+}
+
+local STLDeathSounds = {
+    ["Beveryman"] = STLSoundFolder .. "NixonGrumble.mp3",
+    ["Bombola"] = STLSoundFolder .. "KennedySpirit.mp3",
+    ["Crumbles"] = STLSoundFolder .. "KennedySpirit.mp3",
+    ["Funkyphantom"] = STLSoundFolder .. "MarioDeath.mp3",
+    ["Harumph"] = STLSoundFolder .. "MarioDeath.mp3",
+    ["Heboric"] = STLSoundFolder .. "Wiggam.mp3",
+    ["Luger"] = STLSoundFolder .. "Wiggam.mp3",
+    ["Panserbjørn"] = STLSoundFolder .. "ComingRightForUs.mp3",
+    ["Powerthirst"] = STLSoundFolder .. "NixonGrumble.mp3",
+    ["Rawberry"] = STLSoundFolder .. "NixonGrumble.mp3",
+    ["Resaris"] = STLSoundFolder .. "Wiggam.mp3",
+    ["Shmeeshmaam"] = STLSoundFolder .. "MarioDeath.mp3",
+}
+
+function STLClassColor(charName)
+    return STLClassColors[STLCharacters[charName]]
+end
 
 local f = CreateFrame("Frame")
 f:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
@@ -39,123 +99,35 @@ function f:OnEvent(event, ...)
     -- Spell Effects
     --
     if spellName == "Polymorph" and sourceName == "Powerthirst" then
-        local MSG_POLYMORPH_EFFECT = "|cff40c7eb%s *IS* turtley enough for the Turtle Club!"
-        print(MSG_POLYMORPH_EFFECT:format(destName))
-        PlaySoundFile("Interface\\AddOns\\STLSoundBoard\\Sounds\\TurtleTurtle.mp3", "Master")
+        local MSG_POLYMORPH_EFFECT = "|c%s%s *IS* turtley enough for the Turtle Club!"
+        print(MSG_POLYMORPH_EFFECT:format(STLClassColor(sourceName), destName))
+        PlaySoundFile(STLSoundFolder .. "TurtleTurtle.mp3", STLSoundChannel)
     end
 
     --
     -- Deaths
     --
-    if recapId then
-        if destName == "Powerthirst" or destName == "Beveryman" or destName == "Rawberry" then
-            PlaySoundFile("Interface\\AddOns\\STLSoundBoard\\Sounds\\NixonGrumble.mp3", "Master")
-        end
-        if destName == "Crumbles" or destName == "Bombola" then
-            PlaySoundFile("Interface\\AddOns\\STLSoundBoard\\Sounds\\KennedySpirit.mp3", "Master")
-        end
-        if destName == "Heboric" or destName == "Luger" or destName == "Resaris" then
-            PlaySoundFile("Interface\\AddOns\\STLSoundBoard\\Sounds\\Wiggam.mp3", "Master")
-        end
-        if destName == "Shmeeshmaam" or destName == "Harumph" then
-            PlaySoundFile("Interface\\AddOns\\STLSoundBoard\\Sounds\\MarioDeath.mp3", "Master")
-        end
-        if destName == "Panserbjørn" then
-            PlaySoundFile("Interface\\AddOns\\STLSoundBoard\\Sounds\\ComingRightForUs.mp3", "Master")
-        end
+    if recapId and STLDeathSounds[destName] then
+        PlaySoundFile(STLDeathSounds[destName], STLSoundChannel)
+    end
+
+    --
+    -- Healing
+    --
+    if critical and subevent == "SPELL_HEAL" and STLHealSounds[sourceName] and STLCharacters[destName] then
+        local action = spellName
+        local MSG_CRITICAL_HIT = "|c%s%s's %s critically healed %s for %d health!"
+        print(MSG_CRITICAL_HIT:format(STLClassColor(sourceName), sourceName, action, destName, amount))
+        PlaySoundFile(STLHealSounds[sourceName], STLSoundChannel)
     end
 
     --
     -- Critical Hits
     --
-    if critical and sourceName == "Beveryman" then
+    if critical and STLCritSounds[sourceName] then
         local action = spellName or MELEE
-        local MSG_CRITICAL_HIT = "|cffc79c6e%s's %s critically hit %s for %d damage!"
-        print(MSG_CRITICAL_HIT:format(sourceName, action, destName, amount))
-        PlaySoundFile("Interface\\AddOns\\STLSoundBoard\\Sounds\\Powerthirst.mp3", "Master")
+        local MSG_CRITICAL_HIT = "|c%s%s's %s critically hit %s for %d damage!"
+        print(MSG_CRITICAL_HIT:format(STLClassColor(sourceName), sourceName, action, destName, amount))
+        PlaySoundFile(STLCritSounds[sourceName], STLSoundChannel)
     end
-
-    if critical and sourceName == "Bombola" then
-        local action = spellName or MELEE
-        local MSG_CRITICAL_HIT = "|cfffff569%s's %s critically hit %s for %d damage!"
-        print(MSG_CRITICAL_HIT:format(sourceName, action, destName, amount))
-        PlaySoundFile("Interface\\AddOns\\STLSoundBoard\\Sounds\\Zap.mp3", "Master")
-    end
-
-    if critical and sourceName == "Crumbles" then
-        local action = spellName or MELEE
-        local MSG_CRITICAL_HIT = "|cffa9d271%s's %s critically hit %s for %d damage!"
-        print(MSG_CRITICAL_HIT:format(sourceName, action, destName, amount))
-        PlaySoundFile("Interface\\AddOns\\STLSoundBoard\\Sounds\\Snip_Snap.mp3", "Master")
-    end
-
-    if critical and sourceName == "Funkyphantom" then
-        local action = spellName or MELEE
-        local MSG_CRITICAL_HIT = "|cffc79c6e%s's %s critically hit %s for %d damage!"
-        print(MSG_CRITICAL_HIT:format(sourceName, action, destName, amount))
-        PlaySoundFile("Interface\\AddOns\\STLSoundBoard\\Sounds\\Funky.mp3", "Master")
-    end
-
-    if critical and sourceName == "Harumph" then
-        local action = spellName or MELEE
-        local MSG_CRITICAL_HIT = "|cff8787ed%s's %s critically hit %s for %d damage!"
-        print(MSG_CRITICAL_HIT:format(sourceName, action, destName, amount))
-        PlaySoundFile("Interface\\AddOns\\STLSoundBoard\\Sounds\\Harumph.mp3", "Master")
-    end
-
-    if critical and sourceName == "Heboric" and subevent == "SPELL_HEAL" and (destName == "Beveryman" or destName == "Crumbles" or destName == "Funkyphantom" or destName == "Harumph" or destName == "Heboric" or destName == "Panserbjørn" or destName == "Powerthirst" or destName == "Rawberry" or destName == "Shmeeshmaam") then
-        local action = spellName
-        local MSG_CRITICAL_HIT = "|cffffffff%s's %s critically healed %s for %d health!"
-        print(MSG_CRITICAL_HIT:format(sourceName, action, destName, amount))
-        PlaySoundFile("Interface\\AddOns\\STLSoundBoard\\Sounds\\Hallelujah.mp3", "Master")
-    end
-
-    if critical and sourceName == "Luger" then
-        local action = spellName or MELEE
-        local MSG_CRITICAL_HIT = "|cffa9d271%s's %s critically hit %s for %d damage!"
-        print(MSG_CRITICAL_HIT:format(sourceName, action, destName, amount))
-        PlaySoundFile("Interface\\AddOns\\STLSoundBoard\\Sounds\\GunShow.mp3", "Master")
-    end
-
-    if critical and sourceName == "Resaris" then
-        local action = spellName or MELEE
-        local MSG_CRITICAL_HIT = "|cfffff569%s's %s critically hit %s for %d damage!"
-        print(MSG_CRITICAL_HIT:format(sourceName, action, destName, amount))
-        PlaySoundFile("Interface\\AddOns\\STLSoundBoard\\Sounds\\GunShow.mp3", "Master")
-    end
-
-    if critical and sourceName == "Panserbjørn" then
-        local action = spellName or MELEE
-        local MSG_CRITICAL_HIT = "|cffff7d0a%s's %s critically hit %s for %d damage!"
-        print(MSG_CRITICAL_HIT:format(sourceName, action, destName, amount))
-        PlaySoundFile("Interface\\AddOns\\STLSoundBoard\\Sounds\\Angry_Cat.mp3", "Master")
-    end
-
-    if critical and sourceName == "Powerthirst" then
-        local action = spellName or MELEE
-        local MSG_CRITICAL_HIT = "|cff40c7eb%s's %s critically hit %s for %d damage!"
-        print(MSG_CRITICAL_HIT:format(sourceName, action, destName, amount))
-        PlaySoundFile("Interface\\AddOns\\STLSoundBoard\\Sounds\\Powerthirst.mp3", "Master")
-    end
-
-    if critical and sourceName == "Rawberry" then
-        local action = spellName or MELEE
-        local MSG_CRITICAL_HIT = "|cffa9d271%s's %s critically hit %s for %d damage!"
-        print(MSG_CRITICAL_HIT:format(sourceName, action, destName, amount))
-        PlaySoundFile("Interface\\AddOns\\STLSoundBoard\\Sounds\\Powerthirst.mp3", "Master")
-    end
-
-    if critical and sourceName == "Shmeeshmaam" then
-        local action = spellName or MELEE
-        local MSG_CRITICAL_HIT = "|cfff58cba%s's %s critically hit %s for %d damage!"
-        print(MSG_CRITICAL_HIT:format(sourceName, action, destName, amount))
-        PlaySoundFile("Interface\\AddOns\\STLSoundBoard\\Sounds\\I_Am_The_Law.mp3", "Master")
-    end
-
-    --if not critical and sourceName == "Shmeetest" and subevent == "SPELL_HEAL" and (destName == "Crumbles" or destName == "Funkyphantom" or destName == "Heboric" or destName == "Panserbjørn" or destName == "Powerthirst" or destName == "Shmeeshmaam" or destName == "Shmeetest") then
-    --local action = spellName
-    --local MSG_CRITICAL_HIT = "|cffffffff%s's %s critically healed %s for %d health!"
-    --print(MSG_CRITICAL_HIT:format(sourceName, action, destName, amount))
-    --PlaySoundFile("Interface\\AddOns\\STLSoundBoard\\Sounds\\Harumph.mp3", "Master")
-    --end
 end
